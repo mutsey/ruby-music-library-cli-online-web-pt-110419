866958bc55fcd2d7acfba757b576390f5bb5592f
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-11 23:54:25 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,23 @@ module Concerns
 end
 
 require_all 'lib'
+
+Class: Artist
+ attr_accessor :name, :genre
+ 
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
+end
+
+Class: songs
+attr_accessor :name 
+
+Class: Genre
+attr_accessor :name, :song  
+drake = Artist.new("Drake", "rap")
+hotline_bling = Song.new("Hotline Bling")
+ 
+hotline_bling.artist = drake
