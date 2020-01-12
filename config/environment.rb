34552074cd69632a5cb3acb8c58992484f984507
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-12 01:53:15 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,30 @@ module Concerns
 end
 
 require_all 'lib'
+
+Class: Artist
+ attr_accessor :name, :genre
+ 
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
+end
+
+Class: songs
+ attr_accessor :title, :artist
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+end
+
+
+Class: Genre
+attr_accessor :name, :song, :artist
+drake = Artist.new("Lizzo", "rap")
+Good_as_hell = Song.new("Good_as_hell")
+ 
+Good_as_hell.artist = Lizzo
